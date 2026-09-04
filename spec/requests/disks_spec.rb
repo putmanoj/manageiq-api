9df34d476504ced5a188f39093725ea90f7bf2ff
Gilbert Cherrie <g.cherrie@outlook.com>
2026-09-04 13:11:30 -0400

    Add the disks subcollection to template

--- spec/requests/disks_spec.rb
+++ spec/requests/disks_spec.rb
@@ -3,6 +3,56 @@ RSpec.describe "Disks API" do
   let(:vm) { FactoryBot.create(:vm_vmware, :hardware => hw) }
   let!(:disk) { FactoryBot.create(:disk, :hardware => hw) }
 
+  describe "as a subcollection of Templates" do
+    let(:template_hw) { FactoryBot.create(:hardware) }
+    let(:template) { FactoryBot.create(:template_vmware, :hardware => template_hw) }
+    let!(:template_disk) { FactoryBot.create(:disk, :hardware => template_hw) }
+
+    describe "GET /api/templates/:c_id/disks" do
+      it "can list the disks of a Template" do
+        api_basic_authorize(subcollection_action_identifier(:templates, :disks, :read, :get))
+
+        _other_disk = FactoryBot.create(:disk)
+
+        get(api_template_disks_url(nil, template))
+
+        expected = {
+          "count"     => 2,
+          "name"      => "disks",
+          "subcount"  => 1,
+          "resources" => [
+            {"href" => api_template_disk_url(nil, template, template_disk)}
+          ]
+        }
+
+        expect(response.parsed_body).to include(expected)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    describe "GET /api/templates/:c_id/disks/:s_id" do
+      it "can show a Template's disk" do
+        api_basic_authorize(subcollection_action_identifier(:templates, :disks, :read, :get))
+
+        get(api_template_disk_url(nil, template, template_disk))
+
+        expected = {
+          "href" => api_template_disk_url(nil, template, template_disk),
+          "id"   => template_disk.id.to_s,
+        }
+        expect(response.parsed_body).to include(expected)
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "will not show a disk unless authorized" do
+        api_basic_authorize
+
+        get(api_template_disk_url(nil, template, template_disk))
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
   describe "as a subcollection of VMs" do
     describe "GET /api/vms/:c_id/disks" do
       it "can list the snapshots of a VM" do
